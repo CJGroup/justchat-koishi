eeb4527590a697296d943f6883280eb368228dce
ConstasJ <constasj@qq.com>
2023-06-16 13:25:19 +0800

    fix: small axe

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,7 @@ import { Universal } from "koishi";
 import { v5 } from 'uuid';
 
 export const adaptUser = (user: User): Universal.User => ({
+    selfId: user.uuid?user.uuid:v5(user.name,v5.DNS),
     userId: user.uuid?user.uuid:v5(user.name,v5.DNS),
     username: user.name,
     nickname: user.name,
